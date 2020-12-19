88d09678e9132714472f07d1245b036297cf5307
JerryKaura <jerimkaura001@gmail.com>
2020-12-19 20:25:59 +0300

    Bugs & Fixes

--- member.pl
+++ member.pl
@@ -1,12 +1,10 @@
-
-read_members:-
-    csv_read_file('members.csv', Rows, [functor(table), arity(6)]),
+read_members :-
+    csv_read_file('members.csv', Rows, [functor(vehicle), arity(5)]),
     maplist(assert, Rows).
 
 add_member:-
-    findall(row(ID, FNAME, LNAME, VEHICLE_REG_NO,SHARES,LOAN_STATUS),
-    member(ID, FNAME, LNAME, VEHICLE_REG_NO, SHARES, LOAN_STATUS), Rows),
-      csv_write_file('members.csv', Rows).
+    findall(row(ID, FNAME, LNAME,SHARES,LOAN_STATUS),    member(ID, FNAME, LNAME, SHARES, LOAN_STATUS), Rows),
+    csv_write_file('members.csv', Rows).
 
 register_member:-
     format("\nEnter member ID Number:\n"),
@@ -15,31 +13,29 @@ register_member:-
     read(FNAME),
     format("Enter Member's  Last Name:\n"),
     read(LNAME),
-    format("Enter Member's Vehicle Reg. No:\n"),
-    read(VEHICLE_REG_NO),
     format("Enter Member's shares count:\n"),
     read(SHARES),
-    LOAN_STATUS is 0,
-    assertz(member(ID, FNAME, LNAME, VEHICLE_REG_NO,SHARES, LOAN_STATUS)),
+    LOAN_STATUS is 400,
+    assertz(member(ID, FNAME, LNAME,SHARES, LOAN_STATUS)),
     add_member.
 
 show_members:-
-    format("\n\nID_NUMBER\tFIRST_NAME.\tLAST_NAME\tVEHICLE_REG_NO\tSHARES\tLOAN_STATUS\n"),
-    forall(member(ID, FNAME, LNAME, VEHICLE_REG_NO,SHARES, LOAN_STATUS),
-    format("~w\t\t~w\t\t~w\t\t~w\t\t~w\t\t~w~n", [ID, FNAME, LNAME, VEHICLE_REG_NO,SHARES, LOAN_STATUS])).
-   
+    format("\n\nID_NUMBER\tFIRST_NAME.\tLAST_NAME\tSHARES\tLOAN_STATUS\n"),
+    forall(member(ID, FNAME, LNAME,SHARES, LOAN_STATUS),
+    format("~w\t\t~w\t\t~w\t\t~w\t~w~n", [ID, FNAME, LNAME,SHARES, LOAN_STATUS])).
+
 
 search_member:-
-    format("\nEnter member ID Number:\n"),
+    format("\nEnter member's ID Number:\n"),
     read(X),
-    format("\n\nID_NUMBER\tFIRST_NAME.\tLAST_NAME\tVEHICLE_REG_NO\tSHARES\tLOAN_STATUS\n"),
-    forall(member(X,FNAME, LNAME, VEHICLE_REG_NO,SHARES, LOAN_STATUS),
-    format("~w\t\t~w\t\t~w\t\t~w\t\t~w\t\t~w~n", [X, FNAME, LNAME, VEHICLE_REG_NO,SHARES, LOAN_STATUS])).
+    format("\n\nID_NUMBER\tFIRST_NAME.\tLAST_NAME\tSHARES\tLOAN_STATUS\n"),
+    forall(member(X, FNAME, LNAME,SHARES, LOAN_STATUS),
+    format("~w\t\t~w\t\t~w\t\t~w\t~w~n", [X, FNAME, LNAME,SHARES, LOAN_STATUS])).
 
 members_with_loans:-
-    format("\n\nID_NUMBER\tFIRST_NAME.\tLAST_NAME\tVEHICLE_REG_NO\tSHARES\tLOAN_STATUS\n"),
-    forall(member(ID,FNAME, LNAME, VEHICLE_REG_NO,SHARES, L),L>0,
-    format("~w\t\t~w\t\t~w\t\t~w\t\t~w\t\t~w~n", [ID, FNAME, LNAME, VEHICLE_REG_NO,SHARES, L])).
+    format("\n\nID_NUMBER\tFIRST_NAME.\tLAST_NAME\tSHARES\tLOAN_STATUS\n"),
+    forall(member(ID, FNAME, LNAME,SHARES, X) X is > 1,
+    format("~w\t\t~w\t\t~w\t\t~w\t~w~n", [ID, FNAME, LNAME,SHARES, LOAN_STATUS])).
 
 
 
